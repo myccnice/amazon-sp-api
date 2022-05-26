415c9f3ceb2df812a0dfbf6971ed3b9f4deae068
wangpeng <wangpeng@51web.com>
2022-05-26 09:23:40 +0800

    #0526:去掉敏感信息

--- src/test/java/com/amazon/spapi/api/BaseTest.java
+++ src/test/java/com/amazon/spapi/api/BaseTest.java
@@ -15,11 +15,11 @@ public class BaseTest {
     protected CredentialConfigDTO getJingTongYunConfig() {
         CredentialConfigDTO dto = new CredentialConfigDTO();
         dto.setAccessKeyId("AKIA2DLNGWT4ZVTYO2IT");
-        dto.setSecretKey("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");
+        dto.setSecretKey("xxx");
         dto.setRoleArn("arn:aws:iam::694403118329:role/iam_role");
         dto.setClientId("amzn1.application-oa2-client.10212ced02014df1a79a783777a3fcf3");
         dto.setClientSecret("18e5aff8a6a5d2d94a54fb4c5e4b11cd657a1880485ec81d4aeaa6721c2ba3c7");
-        dto.setRefreshToken("Atzr|IwEBIJZbNjKZAbFw4oCphFxaaZkCyf_fIVJ-571Cv4dERy_UubQwg3Yo8QzHyAvvchdweFFkdgI6_ZK4QpXPhiLD_OC_kNH-zRdQcYyU6FyvOXH3HTcyp-yKYolUV3QWKhKtlCBbhH5S_2UCEsWwTKDPJL1-n-ak6ovnBHz6uVMnmESlbACPMj0rPZOrSmgmU5UUHOolO5IFFEDZ0pg_zN9dLO-ncdtRNwh8eMncBiKxIoDPykK0MpfmftADNbPo8N7E9HN-emc-YA58Sd_dVbL5BGMngk6PG26LL3kmiAHJJUcBQbPBsPiltlhGVsbx8dRKrOE");
+        dto.setRefreshToken("xxx");
         dto.setCountry(String.valueOf(MarketplaceId.US.getId()));
         return dto;
     }
